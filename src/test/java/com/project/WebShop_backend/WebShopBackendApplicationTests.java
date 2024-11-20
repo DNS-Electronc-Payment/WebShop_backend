55e08e41a518ccccc5200047db4e25aee2a80e08
Ninoli1 <nikolinaskiljevic@gmail.com>
2024-11-20 22:47:43 +0100

    Initial Setup

---
+++ src/test/java/com/project/WebShop_backend/WebShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.WebShop_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebShopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
