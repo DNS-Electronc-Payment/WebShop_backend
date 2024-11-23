a4e6a53825824762e928318ea000a08e26bb1d28
Ninoli1 <nikolinaskiljevic@gmail.com>
2024-11-23 11:06:50 +0100

    feat: added https RestTemplate

---
+++ src/main/java/com/project/WebShop_backend/config/RestTemplateConfig.java
@@ -0,0 +1,28 @@
+package com.project.WebShop_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.*;
+import java.security.cert.X509Certificate;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate() throws Exception {
+        // Trust all certificates (self-signed support)
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, new TrustManager[]{new X509TrustManager() {
+            public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+            public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+            public X509Certificate[] getAcceptedIssuers() { return null; }
+        }}, new java.security.SecureRandom());
+
+        HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
+        HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
+
+        return new RestTemplate();
+    }
+}
